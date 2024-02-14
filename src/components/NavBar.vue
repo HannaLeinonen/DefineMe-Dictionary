788ca874c0ed284a1324612b409327fc11d88eb8
HannaLeinonen <120415040+HannaLeinonen@users.noreply.github.com>
2024-02-14 14:03:33 +0100

    Added icon and navbar component

---
+++ src/components/NavBar.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { RouterLink, RouterView} from 'vue-router'
+import BookIcon from '../components/icons/BookIcon.vue'
+</script>
+
+<template>
+    <header>
+        <nav>
+            <div class="containerFluid">
+                <!-- Brand name as link to home page -->
+                <RouterLink class="brandName">DefineMe</RouterLink>
+
+                <!-- Brand icon -->
+                <BookIcon />
+            </div>
+        </nav>
+    </header>
+    
+    <!-- RouterView to render the component -->
+    <RouterView />
+</template>
+
+<style>
+
+</style>
