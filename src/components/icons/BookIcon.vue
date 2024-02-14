1f40ad6bf032d57af1ea9851f0d758d6e95694ad
HannaLeinonen <120415040+HannaLeinonen@users.noreply.github.com>
2024-02-14 15:18:59 +0100

    Added template for HomeView, styled with background image. Added image folder with the background image and new icon components files.

--- src/components/icons/BookIcon.vue
+++ src/components/icons/BookIcon.vue
@@ -1,5 +1,28 @@
 <template>
-    <svg fill="#000000" width="800px" height="800px" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-        <path d="M6.012 18H21V4c0-1.103-.897-2-2-2H6c-1.206 0-3 .799-3 3v14c0 2.201 1.794 3 3 3h15v-2H6.012C5.55 19.988 5 19.806 5 19c0-.101.009-.191.024-.273.112-.576.584-.717.988-.727zM8.648 7.642a2.224 2.224 0 0 1 3.125 0l.224.219.223-.219a2.225 2.225 0 0 1 3.126 0 2.129 2.129 0 0 1 0 3.069L11.998 14l-3.349-3.289a2.128 2.128 0 0 1-.001-3.069z"/>
-    </svg>
+  <svg
+    fill="#E6D5CE"
+    width="34px"
+    height="34px"
+    viewBox="0 0 24.00 24.00"
+    xmlns="http://www.w3.org/2000/svg"
+    stroke="#E6D5CE"
+    stroke-width="0.00024000000000000003"
+  >
+    <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+    <g
+      id="SVGRepo_tracerCarrier"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      stroke="#CCCCCC"
+      stroke-width="0.24000000000000005"
+    ></g>
+    <g id="SVGRepo_iconCarrier">
+      <path
+        d="M3 8v11c0 2.201 1.794 3 3 3h15v-2H6.012C5.55 19.988 5 19.806 5 19s.55-.988 1.012-1H21V4c0-1.103-.897-2-2-2H6c-1.206 0-3 .799-3 3v3zm3-4h13v12H5V5c0-.806.55-.988 1-1z"
+      ></path>
+      <path
+        d="m11.997 14 3.35-3.289a2.129 2.129 0 0 0 0-3.069 2.225 2.225 0 0 0-3.126 0l-.224.218-.224-.219a2.224 2.224 0 0 0-3.125 0 2.129 2.129 0 0 0 0 3.069L11.997 14z"
+      ></path>
+    </g>
+  </svg>
 </template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,41 @@
 <script setup>
-/* import TheWelcome from '../components/TheWelcome.vue' */
+import BookIcon from '../components/icons/BookIcon.vue'
+/* import ForwardIcon from '../components/icons/ForwardIcon.vue' */
 </script>
 
 <template>
   <main>
-    //
+    <div id="backgroundImage">
+        <div class="containerFluid">
+            <h1>DefineMe</h1>
+            <h2>English Dictionary</h2>
+
+            <BookIcon />
+
+            <!-- Forward button to navigate to SearchView -->
+            <!-- <ForwardIcon /> -->
+        </div>
+    </div>
+
   </main>
 </template>
+
+<style scoped>
+    #backgroundImage {
+        margin-top: 0;
+        height: 100vh;
+        background-image: url(../assets/img/city.jpg);
+        background-color: #2A3340;
+        background-position: center;
+        background-repeat: no-repeat;
+        background-size: cover;
+    }
+
+    .containerFluid {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        padding-top: 35vh;
+        color: white;
+    }
+</style>
