924f2e9009d6483f400f2f222d7dbe1b0352abfb
HannaLeinonen <120415040+HannaLeinonen@users.noreply.github.com>
2024-02-19 17:09:26 +0100

    Added function to render random words from dictionary.json and some smaller edits

---
+++ src/components/SearchBar.vue
@@ -0,0 +1,54 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="container">
+    <!-- Form get the word to define -->
+    <form id="searchForm" >
+      <input
+        type="text"
+        id="searchInput"
+
+
+        placeholder="Search for a word"
+      />
+      <button
+      id="searchBtn"
+      aria-label="Search"
+      @click="searchWord()"
+      ><i class="bi bi-search"></i>
+    </button>
+    </form>
+
+
+  </div>
+</template>
+
+
+
+
+<style scoped>
+#searchForm {
+  width: 100%;
+  margin: 1.1rem;
+}
+#searchInput {
+  border: none; /* Removing border for a cleaner look */
+  outline: none;
+  width: 85%;
+  height: 5vh;
+  padding-left: 1rem;
+  border-radius: 0.5rem 0 0 0.5rem;
+}
+#searchBtn {
+  border: none; /* Removing border for a cleaner look */
+  border-radius: 0 0.5rem 0.5rem 0;
+  outline: none;
+  height: 5vh;
+}
+#searchBtn .bi {
+  opacity: 70%;
+}
+
+</style>
--- src/views/SearchView.vue
+++ src/views/SearchView.vue
@@ -1,9 +1,71 @@
+<script setup>
+    import NavBar from '@/components/NavBar.vue';
+    import SearchBar from '@/components/SearchBar.vue';
+    import PopularWords from '@/components/PopularWords.vue';
+    /* import { ref } from 'vue'; */
+
+
+</script>
+
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+    <NavBar />
+
+    <SearchBar />
+
+    <div class="container">
+        <h4>{{ title }}</h4>
+        <ul>
+            <PopularWords v-for="term in popularWords" :key="term" :terms="term"/>
+        </ul>
+    </div>
 </template>
 
-<style>
+<script>
+    export default {
+        data() {
+            return {
+                /* Using the PopularWords-component to save the words input from SearchBar-component in an array */
+                dictionary: [],
+                title: 'Popular words'
+            }
+        },
+        name: 'SearchView',
+        created() {
+            this.fetchDictionary()
+        },
+        computed: {
+            popularWords() {
+                // Shuffle the dictionary array
+            const dictionaryCopy = [...this.dictionary];
+            // Shuffle the copied array
+            const shuffled = this.shuffleArray(dictionaryCopy);
+            // Slice the first 4 elements
+            return shuffled.slice(0, 4);
+            }
+        },
+        methods: {
+            async fetchDictionary() {
+                const response = await fetch('dictionary.json')
+                const value = await response.json()
+                this.dictionary = value
+            },
+            shuffleArray(array) {
+                // Fisher-Yates shuffle algorithm
+                for (let i = array.length - 1; i > 0; i--) {
+                    const j = Math.floor(Math.random() * (i + 1));
+                    [array[i], array[j]] = [array[j], array[i]];
+                }
+                return array;
+            },
+            searchWord() {
+                // Byt ut '/home/result' mot den faktiska sökvägen till den nya sidan
+                window.location.href = '/home/result';
+            }
+        }
+
+    }
+</script>
+
+<style scoped>
 
 </style>
