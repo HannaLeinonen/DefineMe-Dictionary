181753550fba0753b4303d9d4565fa3bd8185361
HannaLeinonen <120415040+HannaLeinonen@users.noreply.github.com>
2024-02-19 17:10:28 +0100

    Changed name of component and made some changes in structure

---
+++ src/components/PopularWords.vue
@@ -0,0 +1,23 @@
+<template>
+    <!-- List all words user searched recently -->
+    <li>{{ terms.word }}</li>
+</template>
+
+<script>
+
+    export default {
+        props: {
+            terms: {
+                required: true,
+                type: Object
+            }
+        }
+    }
+
+</script>
+
+<style>
+ul {
+    list-style: none;
+}
+</style>
